604042aa00ae11f66c5e0f202ccdb575b003fef2
diecgrillo Windows <diecgrillo@gmail.com>
2017-05-22 13:59:37 -0300

    test

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     // we're connected!
-    console.log("Connected correctly to the server: " + url);
+    console.log("Connected correctly to server: " + url);
 });
 
 var index = require('./routes/index');
