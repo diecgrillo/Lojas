c2ef8a0ead398205afdb4dbb22c102d02462bc59
diecgrillo Windows <diecgrillo@gmail.com>
2017-05-22 14:45:50 -0300

    logs

--- app.js
+++ app.js
@@ -11,11 +11,14 @@ var eps     = require('ejs');
 var connection_string = '127.0.0.1:27017/template';
 // if OPENSHIFT env variables are present, use the available connection info:
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+	console.log("###############1");
+	
   connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
   process.env.OPENSHIFT_APP_NAME;
+  console.log("###############2" + connection_string);
 }
 
 
