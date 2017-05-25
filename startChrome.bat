d5b69c4877121f36907d313f1bca3bcd7e17afbb
diecgrillo Windows <diecgrillo@gmail.com>
2017-05-25 15:25:45 -0300

    favicon

--- startChrome.bat
+++
@@ -1,1 +0,0 @@
-chrome.exe --user-data-dir="C:/Chrome dev session" --disable-web-security http:\\localhost:8010/app/#
--- startChrome.sh
+++
@@ -1,1 +0,0 @@
-google-chrome --user-data-dir="/home/diego/Chrome dev session" --disable-web-security http:\\localhost:8010/app/#
